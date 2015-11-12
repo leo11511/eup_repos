47bd0db1a2e38d08014fedff7f628adb60beced6
leo11511 <Leo@Daniels-MacBook-Pro-2.local>
2015-11-12 09:05:43 +0100

    neue Infos

---
+++ rails/rails_app/app/controllers/events_controller.rb
@@ -0,0 +1,7 @@
+class EventsController < ApplicationController
+
+	def index
+		@events = %w(Arran Laphroig Lagavulin)
+	end
+
+end
--- ruby/code/spater_loschen.rb
+++ ruby/code/spater_loschen.rb
@@ -1,5 +1,5 @@
 liste = %w(Hallo Tschüüs Willkommen)
 var = liste.select do |c|
- c.downcase.include?("ll") 
+ c.downcase.include?("mm") 
 end
 puts var
